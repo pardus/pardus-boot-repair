50c0165c491ef10f6cba04ffb0686215d5eb5d8b
vilez <medip642@gmail.com>
2024-04-30 06:24:35 +0300

    refactor: style: feat: new ui with gtk3 and change file layout

---
+++ src/Main.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import gi
+gi.require_version("Gtk", "3.0")
+gi.require_version("Handy", "1")
+from gi.repository import Gtk, Handy, GLib, Gio
+
+Handy.init()
+
+APP_DIR = os.path.dirname(os.path.realpath(__file__))
+
+resource = Gio.resource_load(APP_DIR + "/data/tr.org.pardus.boot-repair.gresource")
+Gio.Resource._register(resource)
+
+class Application(Gtk.Application):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+          *args, 
+          application_id="tr.org.pardus.boot-repair",
+          **kwargs
+        )
+        self.builder = Gtk.Builder()
+        self.builder.add_from_resource("/tr/org/pardus/boot-repair/ui/AppWindow.ui")
+        self.builder.connect_signals(self)
+        self.window = self.builder.get_object("window_main")
+
+    def do_activate(self):
+        self.window.set_application(self)
+        self.window.present()
+
+if __name__ == "__main__":
+    app = Application()
+    app.run(sys.argv)
