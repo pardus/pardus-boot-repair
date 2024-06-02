8b425e75efce7ca4b0d9ab4429db0566d199597c
vilez <medip642@gmail.com>
2024-06-02 16:10:54 +0300

    fix: send environment variables when spawning vte terminal

--- src/Main.py
+++ src/Main.py
@@ -305,8 +305,9 @@ class Application(Gtk.Application):
 
     def vte_command(self, command):
         try:
+            env_vars = [f'{key}={value}' for key, value in os.environ.items()]
             exec = self.vte_terminal.spawn_async(
-                Vte.PtyFlags.DEFAULT, os.environ['HOME'], ["/bin/bash", "-c", command], [], GLib.SpawnFlags.SEARCH_PATH, None, None, -1, None, self.vte_cb)
+                Vte.PtyFlags.DEFAULT, os.environ['HOME'], ["/bin/bash", "-c", command], env_vars, GLib.SpawnFlags.SEARCH_PATH, None, None, -1, None, self.vte_cb)
         except Exception as e:
             # write error to stderr
             sys.stderr.write(str(e) + "\n")
